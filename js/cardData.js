0bbf8019067a206349a6e27f4fcf38db2d10bc69
Chiranjit Behera <99542444+Chiranjit34@users.noreply.github.com>
2022-04-01 12:30:49 +0530

    Update cardData.js

--- js/cardData.js
+++ js/cardData.js
@@ -1,8 +1,8 @@
 var carddetails = [{
     cardnum: "123456789",
     month: "05",
-    year: "25",
-    cvv: "435",
+    year: "29",
+    cvv: "123",
 }]
 
 localStorage.setItem("carddetails",JSON.stringify(carddetails));
